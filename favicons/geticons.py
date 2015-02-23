c29854a622aa353e3dfb89992357f9d44bc2a520
Paul Tarjan <pt@fb.com>
2015-02-23 02:26:31 -0800

    add my new smiley domain

--- favicons/geticons.py
+++ favicons/geticons.py
@@ -3,8 +3,17 @@ import os
 import urllib2
 from BeautifulSoup import BeautifulSoup
 root = 'http://paultarjan.com/sites.php'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    'Accept-Encoding': 'none',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Connection': 'keep-alive',
+}
 
-page = urllib2.urlopen(root)
+req = urllib2.Request(root, headers=headers)
+page = urllib2.urlopen(req)
 soup = BeautifulSoup(page)
 for a in soup('a', rel='me'):
     for img in a('img'):
@@ -17,7 +26,8 @@ for a in soup('a', rel='me'):
                 print 'Skipping %s' % fname
                 continue
             try:
-                data = urllib2.urlopen(src).read()
+                req = urllib2.Request(src, headers=headers)
+                data = urllib2.urlopen(req).read()
                 f = open(fname, 'w')
                 f.write(data)
                 f.close()
