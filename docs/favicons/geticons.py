f9bebd688b1d2bf6410e5ae026886ee739eeae32
Paul Tarjan <github@paulisageek.com>
2022-12-11 15:43:53 -0800

    remove sites.php

--- docs/favicons/geticons.py
+++ docs/favicons/geticons.py
@@ -2,7 +2,7 @@
 import os
 import urllib.request, urllib.error, urllib.parse
 from BeautifulSoup import BeautifulSoup
-root = 'http://paultarjan.com/sites.php'
+root = 'http://paultarjan.com/'
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
